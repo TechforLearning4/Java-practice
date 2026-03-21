d00094d27bfbfe7802a162448fcbdb9464e8d05f
TechforLearning4 <aigpt444@gmail.com>
2026-03-21 16:43:17 +0530

    Update reverse

--- computesSum.java
+++ computesSum.java
@@ -13,11 +13,10 @@ public class computesSum {
     public static int compute(int num){
         int sum = 0;
         while(num>0){
-           sum =sum +num%10;
-           num =num / 10;
+           sum = sum + num % 10;
+           num = num / 10;
 
         }
-
         
         return sum;
     }
---
+++ reverse.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Welcome to reverse of the number: ");
+        System.out.println("Enter the number you want to reverse: ");
+        int num = sc.nextInt();
+        int result = rev(num);
+        System.out.println("The reverse number is: "+ result);
+        sc.close();
+    }
+
+    public static int rev(int num){
+        int newNum =0;
+        int digit = 0;
+        while(0<num){
+            digit =  num % 10;
+            newNum = newNum * 10 + digit;
+            num = num/10;
+        }
+        
+       return newNum;
+    }
+}
