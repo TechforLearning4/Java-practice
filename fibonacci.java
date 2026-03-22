a6a9dcce8dbef7c5c2a45439d4d24f63e8059d4f
TechforLearning4 <aigpt444@gmail.com>
2026-03-22 10:28:13 +0530

    add fibonacci series

--- fibonacci.java
+++ fibonacci.java
@@ -4,11 +4,23 @@ public class fibonacci {
     public static void main(String[] args) {
     Scanner sc =new Scanner(System.in);
     int num = sc.nextInt();
-
+    fib(num);
     sc.close();        
     }
 
     public static int fib(int num){
-        return 0;
+        if(num < 1) return 0;
+        System.out.print("0 ");
+        if(num == 1) return 1;
+        System.out.print("1 ");
+
+        int first = 0, second = 1;
+        while(first + second <= num){
+            int third = first + second;
+            System.out.print(third + " ");
+            first = second;
+            second = third;
+        }
+        return num;
     }
 }
