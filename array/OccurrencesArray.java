dc4dbaa86c22042db89e9fd15e9965837dffaa2c
TechforLearning4 <aigpt444@gmail.com>
2026-04-01 19:58:08 +0530

    Update occurrences java program

--- array/OccurrencesArray.java
+++ array/OccurrencesArray.java
@@ -1,28 +1,36 @@
 package array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
-class OccurrencesArray {
-    public static void main(String[] args) {
+public class OccurrencesArray {
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Array occurrences\n");
-        int[] numArr = YesUtility.createArray();
-        System.out.println("Now enter the number you want to find: ");
-        int num = input.nextInt();
-        int occurrences = noofOccurrences(numArr, num);
-        System.out.println("Your element was found " + occurrences + " times in the array.");
-        input.close();
-    }
-
-    public static int noofOccurrences(int[] numArr, int num) {
-        int occ = 0;
+        System.out.println("Welcome to Occerences of elements inside an array: ");
+        System.out.println("Declare the size of array: ");
+        int size = input.nextInt();
+        int[] nums = new int[size];
         int i = 0;
-        while (i < numArr.length) {
-            if (numArr[i] == num) {
-                occ++;
-            }
+        while(i < size){
+            System.out.print("Enter the elememt number at position "+ (i+1) + ": ");
+            nums[i] = input.nextInt();
             i++;
         }
-        return occ;
+        System.out.println(" " + Arrays.toString(nums));
+        System.out.print("Enter the number you want to find: ");
+        int number = input.nextInt();
+        int occurrences = occurrences(nums , number );
+        System.out.println("Your element was found " + occurrences+ " times inside the array. ");
+        input.close();
+    }
+    
+    public static int occurrences(int[] nums, int number){
+        int count = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] ==  number ){
+                count = count +1;
+            }
+        }
+        return count;
     }
 }
